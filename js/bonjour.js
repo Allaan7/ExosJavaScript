1c7445b72fea202c87a0a99c5aa3405f1ba12741
Allaan7 <allaan.salim@gmail.com>
2024-06-18 13:38:52 +0200

    exos js

--- js/bonjour.js
+++ js/bonjour.js
@@ -7,3 +7,11 @@ nom = prompt("Bonjour, quel est ton nom?")
 console.log(nom);
 prenom = prompt("Et quel est ton prénom?")
 console.log(prenom);
+
+let nom;
+let prenom;
+
+nom = prompt("Entrez votre nom : ");
+prenom = prompt("Entrez votre prénom :");
+
+alert("Bonjour, " + prenom + " " + nom);
---
+++ js/tva.js
@@ -0,0 +1,5 @@
+let tva;
+tva = 47;
+
+tva = tva * 1.2;
+console.log(tva)
